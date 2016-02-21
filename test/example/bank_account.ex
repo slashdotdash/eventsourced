fa18e9e41258af617727ad52d9dc9d84c2a7e76b
Ben Smith <ben@10consulting.com>
2016-02-21 21:19:44 +0000

    Provide aggregate identity to `new` function
    Instead of relying on the aggregate to generate an identity for itself.

--- test/example/bank_account.ex
+++ test/example/bank_account.ex
@@ -23,57 +23,57 @@ defmodule BankAccount do
 
   alias Events.{BankAccountOpened,MoneyDeposited,MoneyWithdrawn}
 
-  def new do
-    %BankAccount{id: UUID.uuid1(), state: %BankAccount.State{}}
+  def new(id) do
+    %BankAccount{id: id, state: %BankAccount.State{}}
   end
 
   def load(id, events) do
-    account = %BankAccount{id: id, state: %BankAccount.State{}}    
+    account = %BankAccount{id: id, state: %BankAccount.State{}}
     Enum.reduce(events, account, &apply(&2, &1))
   end
 
   def open_account(%BankAccount{} = account, account_number, initial_balance) when initial_balance > 0 do
-    account 
-    |> apply(%BankAccountOpened { account_number: account_number, initial_balance: initial_balance })
+    account
+    |> apply(%BankAccountOpened{account_number: account_number, initial_balance: initial_balance})
   end
 
   def deposit(%BankAccount{} = account, amount) do
     balance = account.state.balance + amount
 
-    account 
+    account
     |> apply(%MoneyDeposited{ amount: amount, balance: balance })
   end
 
   def withdraw(%BankAccount{} = account, amount) do
     balance = account.state.balance - amount
 
-    account 
+    account
     |> apply(%MoneyWithdrawn{ amount: amount, balance: balance })
   end
 
   defp apply(%BankAccount{} = account, %BankAccountOpened{} = account_opened) do
-    apply_event(account, account_opened, %{
+    apply_event(account, account_opened, fn state -> %{state |
       account_number: account_opened.account_number,
       balance: account_opened.initial_balance
-    })
+    } end)
   end
 
   defp apply(%BankAccount{} = account, %MoneyDeposited{} = money_deposited) do
-    apply_event(account, money_deposited, %{
+    apply_event(account, money_deposited, fn state -> %{state |
       balance: money_deposited.balance
-    })
+    } end)
   end
 
   defp apply(%BankAccount{} = account, %MoneyWithdrawn{} = money_withdrawn) do
-    apply_event(account, money_withdrawn, %{
+    apply_event(account, money_withdrawn, fn state -> %{state |
       balance: money_withdrawn.balance
-    })
+    } end)
   end
 
-  defp apply_event(%BankAccount{} = account, event, state) do
-    %BankAccount{account | 
+  defp apply_event(%BankAccount{} = account, event, update_state_fn) do
+    %BankAccount{account |
       events: [event | account.events],
-      state: Map.merge(account.state, state),
+      state: update_state_fn.(account.state),
       version: account.version + 1
     }
   end
--- test/example/bank_account_test.exs
+++ test/example/bank_account_test.exs
@@ -5,24 +5,17 @@ defmodule BankAccountTest do
   alias BankAccount.Events.{BankAccountOpened,MoneyDeposited,MoneyWithdrawn}
 
   test "open account" do
-    account = BankAccount.new
+    account = BankAccount.new("123")
       |> BankAccount.open_account("ACC123", 100)
 
+    assert account.id == "123"
     assert account.events == [%BankAccountOpened{account_number: "ACC123", initial_balance: 100}]
     assert account.state == %BankAccount.State{account_number: "ACC123", balance: 100}
-  end
-
-  test "reload from events" do
-    events = [%BankAccountOpened{account_number: "ACC123", initial_balance: 100}]
-    account = BankAccount.load("1234", events)
-
-    assert account.id == "1234"
-    assert account.state == %BankAccount.State{account_number: "ACC123", balance: 100}
-    assert length(account.events) == 1
+    assert account.version == 1
   end
 
   test "deposit money" do
-    account = BankAccount.new
+    account = BankAccount.new("123")
       |> BankAccount.open_account("ACC123", 100)
       |> BankAccount.deposit(50)
 
@@ -31,17 +24,29 @@ defmodule BankAccountTest do
       %MoneyDeposited{amount: 50, balance: 150}
     ]
     assert account.state == %BankAccount.State{account_number: "ACC123", balance: 150}
+    assert account.version == 2
   end
 
   test "withdraw money" do
-    account = BankAccount.new
+    account = BankAccount.new("123")
       |> BankAccount.open_account("ACC123", 100)
       |> BankAccount.withdraw(50)
-      
+
     assert Enum.reverse(account.events) == [
       %BankAccountOpened{account_number: "ACC123", initial_balance: 100},
       %MoneyWithdrawn{amount: 50, balance: 50}
     ]
     assert account.state == %BankAccount.State{account_number: "ACC123", balance: 50}
+    assert account.version == 2
+  end
+
+  test "load from events" do
+    events = [%BankAccountOpened{account_number: "ACC123", initial_balance: 100}]
+    account = BankAccount.load("1234", events)
+
+    assert account.id == "1234"
+    assert account.state == %BankAccount.State{account_number: "ACC123", balance: 100}
+    assert length(account.events) == 1
+    assert account.version == 1
   end
 end
